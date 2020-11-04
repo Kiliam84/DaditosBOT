87f561a4d095e0051e8e6163fea3945ae98efeee
Kiliam84 <73685388+Kiliam84@users.noreply.github.com>
2020-11-04 15:35:24 -0500

    Create webserver.js

---
+++ webserver.js
@@ -0,0 +1,21 @@
+// Web server
+const http = require('http');
+const express = require('express');
+const app = express();
+
+//
+app.use(express.static('public'));
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+
+app.listen(process.env.PORT);
+
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.herokuapp.com/`);
+}, 280000);
