f856e326c6fea919d82db73eb75fb4990b02c30c
Kiliam84 <73685388+Kiliam84@users.noreply.github.com>
2020-11-05 21:13:16 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -14,81 +14,80 @@ var empate = 0;
 var seg = 20;
 
 client.on('ready', () => {
-  console.log('Ready to roll!! :D');
+	console.log('Ready to roll!! :D');
 });
 
 client.on("message", function(message) {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+	  
+	const commandBody = message.content.slice(prefix.length);
+	const args = commandBody.split(' ');
+	const command = args.shift().toLowerCase();
   
-  const commandBody = message.content.slice(prefix.length);
-  const args = commandBody.split(' ');
-  const command = args.shift().toLowerCase();
   
-  
-  if (command === "dados") {
-	dice = args
-	if(esEntero(dice) == false){
-		dice = 100;
-	}
-	if(message.author.username == 'Kharmah'){
-		aleatorio = dice;
-		message.reply(`tira los dados y saca `+dice);
-	}else{
-	aleatorio = Math.round(Math.random()*(dice-1)+1);
-    message.reply(`tira los dados y saca ` + aleatorio + `  (1-`+dice+`)`);
-	}
-	if(rollon == 1){
-		if (aleatorio > maxroll){
-			maxroll = aleatorio;
-			maxroller = message.author.username;
+	if (command === "dados") {
+		dice = commandBody;
+		if(esEntero(dice) == false){
+			dice = 100;
+		}
+		if(message.author.username == 'Kharmah'){
+			aleatorio = dice;
+			message.reply(`tira los dados y saca `+dice);
 		}else{
-			if(aleatorio == maxroll && maxroller != maxroller){
-				maxroller = maxroller + ` y ` + message.author.username;
-				empate = 1;
+			aleatorio = Math.round(Math.random()*(dice-1)+1);
+			message.reply(`tira los dados y saca ` + aleatorio + `  (1-`+dice+`)`);
+		}
+		if(rollon == 1){
+			if (aleatorio > maxroll){
+				maxroll = aleatorio;
+				maxroller = message.author.username;
+			}else{
+				if(aleatorio == maxroll && maxroller != maxroller){
+					maxroller = maxroller + ` y ` + message.author.username;
+					empate = 1;
+				}
 			}
 		}
-    }
-  }
-  
-  if (command === "roleo") {
-	rollon = 1;
-	maxroll = 0;
-	maxroller = null;
-	empate = 0;
-	seg = args;
-	if(esEntero(seg) == false){
-		seg = 20;
 	}
-    message.reply(`ha iniciado una tirada de dados de `+seg+` segundos!`);
-	var time = setInterval(
-		function contador(){ 
-			seg--;
-			if(seg==0){
-				clearInterval(time);
-				if(maxroller == null){
-					message.reply(`Nadie ha participado en la tirada de dados...`);
-				}else{
-					if(empate == 0){
-						message.reply(`El ganador de la tirada es `+maxroller+` con una tirada de `+maxroll);
+  
+	if (command === "roleo") {
+		rollon = 1;
+		maxroll = 0;
+		maxroller = null;
+		empate = 0;
+		seg = args;
+		if(esEntero(seg) == false){
+			seg = 20;
+		}
+		message.reply(`ha iniciado una tirada de dados de `+seg+` segundos!`);
+		var time = setInterval(
+			function contador(){
+				seg--;
+				if(seg==0){
+					clearInterval(time);
+					if(maxroller == null){
+						message.reply(`Nadie ha participado en la tirada de dados...`);
+					}else{
+						if(empate == 0){
+							message.reply(`El ganador de la tirada es `+maxroller+` con una tirada de `+maxroll);
+						}
+						if(empate == 1){
+							message.reply(`Los ganadores de la tirada son `+maxroller+` con una tirada de `+maxroll);
+						}
 					}
-					if(empate == 1){
-						message.reply(`Los ganadores de la tirada son `+maxroller+` con una tirada de `+maxroll);
-					}
+					rollon = 0;
 				}
-				rollon = 0;
-			}
-			if(seg==10){
-				message.reply(`quedan `+seg+` segundos!`)
-			}
-			if(seg<=3 && seg > 0){
-				message.reply(`quedan `+seg+` segundos!`)
-			} 
-		},1000);
-  }
-}
-
-);
+				if(seg==10){
+					message.reply(`quedan `+seg+` segundos!`)
+				}
+				if(seg<=3 && seg > 0){
+					message.reply(`quedan `+seg+` segundos!`)
+				} 
+			},1000
+		);
+	}
+});
 
 function esEntero(x){
 	var y = parseInt(x);
